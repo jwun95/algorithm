9bdbfab447f39b18b634ec13b56f13f3dfae66a2
DARTZ <jwun95@naver.com>
2022-04-29 20:23:24 +0900

    [Silver I] Title: 트리 순회, Time: 120 ms, Memory: 125912 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,37 @@
+import sys
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
+
+graph = []
+
+N = int(input())
+tree = {} # 집합으로 설정
+
+for n in range(N):
+    root, left, right = sys.stdin.readline().strip().split()
+    tree[root] = [left, right]
+
+def preorder(root):
+    if root != '.':
+        print(root, end='')
+        preorder(tree[root][0])
+        preorder(tree[root][1])
+
+def inorder(root):
+    if root != '.':
+        inorder(tree[root][0])
+        print(root, end='')
+        inorder(tree[root][1])
+
+def postorder(root):
+    if root != '.':
+        postorder(tree[root][0])
+        postorder(tree[root][1])
+        print(root, end='')
+
+
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
