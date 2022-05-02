e08561cec6dcb7b0579d7fb1c6ccc7ee39e6660e
DARTZ <jwun95@naver.com>
2022-05-02 21:36:00 +0900

    [Silver II] Title: DFS와 BFS, Time: 184 ms, Memory: 133396 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,41 +1,42 @@
-from collections import deque
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
 N, M, V = map(int, input().split())
 
-dfs_visited = [False] * (N + 1)
-bfs_visited = [False] * (N + 1)
-dfs_graph = [[0] * (N+1) for i in range(N+1)]
-bfs_graph = [[0] * (N+1) for i in range(N+1)]
+dfs_graph = [[0 for i in range(N+1)] for k in range(N+1)]
+bfs_graph = [[0 for i in range(N+1)] for k in range(N+1)]
+dfs_visited = [0] * (N + 1)
+bfs_visited = [0] * (N + 1)
+
 
 for _ in range(M):
-    a, b = map(int, input().split())
-    dfs_graph[a][b] = dfs_graph[b][a] = 1
-    bfs_graph[a][b] = bfs_graph[b][a] = 1
+    x, y = map(int, input().split())
+    dfs_graph[x][y] = dfs_graph[y][x] =1
+    bfs_graph[x][y] = bfs_graph[y][x] = 1
 
 
-def dfs(v):
-    dfs_visited[v] = True
-    print(v, end=" ")
+def dfs(n):
+    dfs_visited[n] = 1
+    print(n, end=" ")
     for i in range(1, N+1):
-        if dfs_visited[i] == False and dfs_graph[v][i] == 1:
+        if dfs_visited[i] == 0 and dfs_graph[n][i] == 1:
             dfs(i)
 
 
-def bfs(v):
-    que = deque([v])
-    bfs_visited[v] = True
+def bfs(n):
+    queue = deque([n])
+    bfs_visited[n] = 1
 
-    while que:
-        start = que.popleft()
-        print(start, end=' ')
+    while queue:
+        num = queue.popleft()
+        print(num, end=" ")
 
         for i in range(N+1):
-            if bfs_graph[start][i] == 1 and bfs_visited[i] == False:
-                bfs_visited[i] = True
-                que.append(i)
+            if bfs_graph[num][i] == 1 and bfs_visited[i] == 0:
+                bfs_visited[i] = 1
+                queue.append(i)
 
 
 dfs(V)
