baae8fa4c4417eee009e9c5b34cc11e2ae562de4
Shim Jae Woon ( DARTZ ) <jwun95@naver.com>
2023-05-15 14:36:41 +0900

    [unrated] Title: 배열 만들기 6, Time: 3.51 ms, Memory: 38 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.js
@@ -0,0 +1,18 @@
+function solution(arr) {
+    var answer = [];
+    let i = 0;
+    
+    while (i< arr.length) {
+        if (answer.length === 0) {
+            answer.push(arr[i]);
+        } else if (answer[answer.length - 1] === arr[i]) {
+            answer.pop();
+        } else {
+            answer.push(arr[i]);
+        }
+        i += 1;
+    }
+    
+    
+    return answer.length === 0 ? [-1] : answer;
+}
