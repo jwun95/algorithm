3f4e9b9a077cbdb1bf6c09b2f58cbd3463dbc457
Shim Jae Woon ( DARTZ ) <jwun95@naver.com>
2023-05-17 23:46:22 +0900

    [unrated] Title: 리스트 자르기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,17 @@
+function solution(n, slicer, num_list) {
+    var answer = [];
+    
+    if (n === 1) {
+        answer = num_list.slice(0, slicer[1] + 1);
+    } else if (n === 2) {
+        answer = num_list.slice(slicer[0]);
+    } else if (n === 3) {
+        answer = num_list.slice(slicer[0], slicer[1] + 1);
+    } else {
+        for (let i = slicer[0]; i < slicer[1] + 1; i = i + slicer[2]) {
+            answer.push(num_list[i]);
+        }
+    }
+    
+    return answer;
+}
