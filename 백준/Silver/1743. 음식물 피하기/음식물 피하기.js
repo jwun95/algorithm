0daef504cca9ab931749561c194b7b249f4d8e95
Shim Jae Woon ( DARTZ ) <jwun95@naver.com>
2023-07-20 15:42:26 +0900

    [Silver I] Title: 음식물 피하기, Time: 144 ms, Memory: 14460 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.js
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const stdin = (
   process.platform === "linux"
     ? fs.readFileSync("/dev/stdin").toString()
-    : fs.readFileSync("../input.txt").toString()
+    : fs.readFileSync("./input.txt").toString()
 ).split("\n");
 
 const input = (() => {
@@ -13,61 +13,62 @@ const input = (() => {
 const [N, M, K] = input()
   .trim()
   .split(" ")
-  .map((v) => parseInt(v));
+  .map((v) => Number(v));
 
+const graph = Array.from(Array(N), () => Array(M).fill(false));
+const visited = Array.from(Array(N), () => Array(M).fill(false));
 const trash = [];
 
 for (let i = 0; i < K; i++) {
-  trash.push(
-    input()
-      .trim()
-      .split(" ")
-      .map((v) => parseInt(v))
-  );
+  const [x, y] = input()
+    .trim()
+    .split(" ")
+    .map((v) => Number(v));
+
+  trash.push([x - 1, y - 1]);
+  graph[x - 1][y - 1] = true;
 }
 
-const area = Array.from(Array(N), () => Array(M).fill(false));
-const visited = Array.from(Array(N), () => Array(M).fill(false));
+function bfs(queue) {
+  const direction = [
+    [0, 1],
+    [0, -1],
+    [1, 0],
+    [-1, 0],
+  ];
+  let count = 1;
+  while (queue.length) {
+    const [x, y] = queue.shift();
 
-const direction = [
-  [0, 1],
-  [0, -1],
-  [1, 0],
-  [-1, 0],
-];
+    for (const d of direction) {
+      const nextX = x + d[0];
+      const nextY = y + d[1];
+
+      if (
+        nextX >= 0 &&
+        nextX < N &&
+        nextY >= 0 &&
+        nextY < M &&
+        !visited[nextX][nextY] &&
+        graph[nextX][nextY]
+      ) {
+        visited[nextX][nextY] = true;
+        count += 1;
+        queue.push([nextX, nextY]);
+      }
+    }
+  }
+  return count;
+}
+
+const answer = [];
 
 for (const t of trash) {
-  area[t[0] - 1][t[1] - 1] = true;
-}
-
-function dfs(y, x, originY, originX) {
-  for (const d of direction) {
-    if (
-      y + d[0] >= 0 &&
-      y + d[0] < N &&
-      x + d[1] >= 0 &&
-      x + d[1] < M &&
-      !visited[y + d[0]][x + d[1]] &&
-      area[y + d[0]][x + d[1]]
-    ) {
-      visited[y + d[0]][x + d[1]] = true;
-      area[originY][originX] += 1;
-      dfs(y + d[0], x + d[1], originY, originX);
-    }
+  if (!visited[t[0]][t[1]]) {
+    visited[t[0]][t[1]] = true;
+    const result = bfs([t]);
+    answer.push(result);
   }
 }
 
-const answerList = [];
-
-for (let y = 0; y < N; y++) {
-  for (let x = 0; x < M; x++) {
-    if (!visited[y][x] && area[y][x]) {
-      visited[y][x] = true;
-      area[y][x] = 1;
-      dfs(y, x, y, x);
-      answerList.push(area[y][x]);
-    }
-  }
-}
-
-console.log(Math.max(...answerList));
+console.log(Math.max(...answer));
