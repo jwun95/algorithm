cab800fdcab16b0431c28d3baf1bac94ea3af2fb
Shim Jae Woon ( DARTZ ) <jwun95@naver.com>
2023-07-26 13:00:50 +0900

    [Gold IV] Title: 탈출, Time: 124 ms, Memory: 11288 KB -BaekjoonHub

---
+++ 백준/Gold/3055. 탈출/탈출.js
@@ -0,0 +1,73 @@
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : fs.readFileSync("./input.txt").toString()
+).split("\n");
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const direction = [
+  [-1, 0],
+  [1, 0],
+  [0, -1],
+  [0, 1],
+];
+
+const [R, C] = input()
+  .trim()
+  .split(" ")
+  .map((v) => Number(v));
+
+const graph = [];
+
+for (let i = 0; i < R; i++) {
+  graph.push(input().trim().split(""));
+}
+
+const S = [0, 0, 1];
+const queue = [];
+const goal = [0, 0];
+
+for (let r = 0; r < R; r++) {
+  for (let c = 0; c < C; c++) {
+    if (graph[r][c] === "S") {
+      S[0] = r;
+      S[1] = c;
+    } else if (graph[r][c] === "*") {
+      queue.push([r, c, 0]);
+    } else if (graph[r][c] === "D") {
+      goal[0] = r;
+      goal[1] = c;
+    }
+  }
+}
+queue.push(S);
+
+console.log(bfs(queue));
+
+function bfs(queue) {
+  let count = 0;
+  while (queue.length > count) {
+    const [r, c, s] = queue[count];
+
+    for (const d of direction) {
+      const nR = r + d[0];
+      const nC = c + d[1];
+
+      if (s !== 0 && nR === goal[0] && nC === goal[1]) return s;
+
+      if (nR >= 0 && nR < R && nC >= 0 && nC < C && graph[nR][nC] === ".") {
+        graph[nR][nC] = "*";
+
+        if (s === 0) queue.push([nR, nC, 0]);
+        else queue.push([nR, nC, s + 1]);
+      }
+    }
+    count++;
+  }
+  return "KAKTUS";
+}
