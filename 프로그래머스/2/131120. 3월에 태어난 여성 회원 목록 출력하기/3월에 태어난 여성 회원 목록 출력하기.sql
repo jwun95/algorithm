3b71988e89f1af3491f0dcb19f41ab19eac7b3c2
Shim Jae Woon ( DARTZ ) <jwun95@naver.com>
2023-10-18 19:16:16 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT MEMBER_ID, MEMBER_NAME, GENDER, DATE_FORMAT(DATE_OF_BIRTH, '%Y-%m-%d') as DATE_OF_BIRTH
+from MEMBER_PROFILE
+where DATE_OF_BIRTH like '%-03-%' and 
+TLNO IS NOT NULL and
+GENDER = 'W'
+order by MEMBER_ID;
