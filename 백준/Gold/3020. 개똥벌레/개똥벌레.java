c0e896a4beb85334fb018e107d22e79407199227
DARTZ <jwun95@naver.com>
2025-02-07 21:07:30 +0900

    [Gold V] Title: 개똥벌레, Time: 256 ms, Memory: 30764 KB -BaekjoonHub

--- 백준/Gold/3020. 개똥벌레/개똥벌레.java
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.java
@@ -1,70 +1,50 @@
 
-
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
-  static int N, H;
-  static int[] bottoms;
-  static int[] ups;
-
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringBuilder sb = new StringBuilder();
 
     StringTokenizer st = new StringTokenizer(br.readLine());
 
-    N = Integer.parseInt(st.nextToken());
-    H = Integer.parseInt(st.nextToken());
-    int count = 0;
-    int standard = Integer.MAX_VALUE;
+    int N = Integer.parseInt(st.nextToken());
+    int H = Integer.parseInt(st.nextToken());
 
-    bottoms = new int[N / 2];
-    ups = new int[N / 2];
+    int[] bottoms = new int[H + 2];
+    int[] tops = new int[H + 2];
 
     for (int n = 0; n < N / 2; n++) {
-      bottoms[n] = Integer.parseInt(br.readLine());
-      ups[n] = Integer.parseInt(br.readLine());
+      bottoms[Integer.parseInt(br.readLine())] += 1;
+      tops[Integer.parseInt(br.readLine())] += 1;
     }
 
-    Arrays.sort(bottoms);
-    Arrays.sort(ups);
+    for (int i = H; i >= 1; i --) {
+      bottoms[i] += bottoms[i + 1];
+      tops[i] += tops[i + 1];
+    }
+
+    int minStandard = Integer.MAX_VALUE;
+    int count = 0;
 
     for (int h = 1; h <= H; h ++) {
-      int bottom = bottoms.length - binarySearch(h, bottoms);
-      int up = ups.length - binarySearch(H - h + 1, ups);
+      int standard = bottoms[h] + tops[H - h + 1];
 
-      if (bottom + up < standard) {
-        standard = bottom + up;
+      if (standard < minStandard) {
+        minStandard = standard;
         count = 1;
-      } else if (bottom + up == standard) count++;
+      } else if (standard == minStandard) count ++;
     }
 
-    sb.append(standard).append(" ").append(count);
+    StringBuilder sb = new StringBuilder();
+
+    sb.append(minStandard).append(" ").append(count);
+
     System.out.println(sb.toString());
 
     br.close();
-  }
-
-  static int binarySearch(int num, int[] arr) {
-    int low = 0;
-    int high = arr.length - 1;
-    int mid = 0;
-
-    while (low <= high) {
-      mid = (low + high) / 2;
-
-      if (arr[mid] >= num) {
-        high = mid - 1;
-      } else {
-        low = mid + 1;
-      }
-    }
-
-    return low;
   }
 }
